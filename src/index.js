217c695a85c2c26ba1a4777f83d8a324bf8c3f4b
Felipe Buenaño <fbuenano>
2017-05-18 13:15:49 -0400

    udpates

--- src/index.js
+++ src/index.js
@@ -38,20 +38,21 @@ const file2 = fileReader('../../src/problem2/problem2.in');
 const outputFile1 = outputReader('../../src/problem1/problem1.out');
 const outputFile2 = outputReader('../../src/problem2/problem2.out');
 
-file1.then((data) => {
-  return problem1(data);
-})
-.then((p1) => {
-  outputFile1.then((out) => {
-    console.log(`is problem1 resolved? ${equal(p1, out)}`);
-  });
-});
+// file1.then((data) => {
+//   return problem1(data);
+// })
+// .then((p1) => {
+//   outputFile1.then((out) => {
+//     console.log(`is problem1 resolved? ${equal(p1, out)}`);
+//   });
+// });
 
 file2.then((data) => {
   return problem2(data);
 })
 .then((p2) => {
   outputFile2.then((out) => {
-    console.log(`is problem2 resolved? ${equal(p2, out)}`);
+    console.log(p2);
+  //   console.log(`is problem2 resolved? ${equal(p2, out)}`);
   });
 });
--- src/problem2/problem2.js
+++ src/problem2/problem2.js
@@ -50,19 +50,52 @@ function checkDigit (digit, digitArr) {
 
 };
 
+function checkValue(str) {
+  var base = 27;
+  var checkArr = [2, 4, 5, 7, 8, 10, 11, 13];
+  var arr = str.split('');
+  var checkValueArr =  arr.map(function(value, index){
+    return value * checkArr[index];
+  });
+
+  var sum = checkValueArr.reduce(function (a, b) {
+    return a + b;
+  });
+
+  return sum%base;
+}
+
+function getDigit(str) {
+  return parseInt(str, 27);
+}
+
+function getOutput(str) {
+  var value = getDigit(str);
+  return parseInt(value, 10);
+}
+
 module.exports.problem2 = function(dataSet) {
 
-  dataSet.forEach(function(str, index){
+  var ds = [dataSet[0]];
 
+  var ds = ['12345678A','12435678A','12355678A'];
+
+  return ds.map(function(str, index){
 
     var data = str.substring(0,9);
-    var checkDigArr = data.substring(0,8);
-    var digit = data.substring(8,9);
+    var checkDigStr = data.substring(0,8);
+    var checkDigit = data.substring(8,9);
 
+    // console.log(checkValue(checkDigStr));
+    // console.log(getDigit(checkDigit));
+    // console.log(getDigit(checkDigit) === checkValue(checkDigStr));
 
-    // var data = str.split('');
-    
-    console.log(parseInt(digit,27));
+    if(getDigit(checkDigit) === checkValue(checkDigStr)) {
+      return getOutput(checkDigStr);
+    }
+    return 'Invalid';
+
+    // console.log(parseInt(digit,27));
 
     // var checkDigitArr = data.splice(0,8);
     // var digit = parseInt(data[0],27);
@@ -77,11 +110,11 @@ module.exports.problem2 = function(dataSet) {
 
     // console.log(checkDigitArr);
     // console.log(digit);
-    
+
 
     // console.log('===> %s',checkDigit(digit, checkDigitArr))
   });
-  
+
 };
 
 // var testData = [
