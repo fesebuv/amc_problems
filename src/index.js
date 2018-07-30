536560d7e6184f771db87c3b4d10fc0a7e9ad8f6
Felipe Buenaño <fesebuv@gmail.com>
2018-07-30 14:18:44 -0400

    improve code

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ const file2 = inputReader('../../src/problem2/problem2.in');
 const outputFile1 = outputReader('../../src/problem1/problem1.out');
 const outputFile2 = outputReader('../../src/problem2/problem2.out');
 
+const files = ['problem1'];
+
+files.forEach(function (file) {
+  console.log(file, '<<< file');
+});
+
 file1
   .then((data) => problem1(data))
   .then((p1) => {
