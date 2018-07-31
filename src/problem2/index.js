582e6c7875734aa1c242e1c0bacb7ea9eb334f42
Felipe Buenaño <fesebuv@gmail.com>
2018-07-31 16:02:19 -0400

    update

--- src/problem2/index.js
+++ src/problem2/index.js
@@ -1,6 +1,6 @@
 const { writeToFile } = require('../helpers/');
 
-var charMap = {
+const charMap = {
   B: '8',
   G: 'C',
   I: '1',
@@ -12,17 +12,16 @@ var charMap = {
   Z: '2'
 };
 
-var base27 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'];
-var givenBase27 = ['0','1','2','3','4','5','6','7','8','9','A','C','D','E','F','H','J','K','L','M','N','P','R','T','V','W','X'];
-
-var checkDigitArr = [2, 4, 5, 7, 8, 10, 11, 13];
+const base27 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'];
+const givenBase27 = ['0','1','2','3','4','5','6','7','8','9','A','C','D','E','F','H','J','K','L','M','N','P','R','T','V','W','X'];
+const checkDigitArr = [2, 4, 5, 7, 8, 10, 11, 13];
 
 function base27To10(str) {
   const num = parseInt(str, 27).toString(10);
   return num * 1;
 }
 
-function cleanData(input) {
+function normalizeData(input) {
   const arr = input.split('');
   return arr
     .map((chr) => {
@@ -54,21 +53,20 @@ function getCheckSum(digitArr) {
 function calculateCheckSum(input) {
   const stringArr = input.split('');
   const checkDigit = stringArr.pop();
-
   const num = base27To10(checkDigit);
   const checkSum = getCheckSum(stringArr);
-
+  const checkPhrase = stringArr.join('');
 
   if(num !== checkSum) {
     return 'Invalid';
   }
-  const checkPhrase = stringArr.join('');
+
   return base27To10(checkPhrase);
 }
 
 function problem2(dataSet) {
   const data = dataSet.map((input, index) => {
-    const cleanInput = cleanData(input);
+    const cleanInput = normalizeData(input);
     const checkSum = calculateCheckSum(cleanInput);
     return `${index + 1} ${checkSum}`;
   });
