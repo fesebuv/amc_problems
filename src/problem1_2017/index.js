7e4ed11d27098495fef12fb9fcc0fd0175b585e3
Felipe Buenano <fesebuv@gmail.com>
2018-08-20 18:37:59 -0400

    include problem 1 from 2017

---
+++ src/problem1_2017/index.js
@@ -0,0 +1,22 @@
+const { writeToFile } = require('../helpers/');
+
+function candlesForhanukah(candles) {
+  var total = 0;
+  for(var i = candles + 1; i >= 2; i-- ) {
+    total = total + i;
+  }
+  return total * 1;
+}
+
+function problem1_2017(dataSet) {
+  const data = dataSet.map((num, index) => {
+    const candles = num * 1;
+    return `${index + 1} ${candlesForhanukah(candles)}`;
+  });
+  writeToFile(data, 'problem1_2017');
+};
+
+
+module.exports =  {
+  default: problem1_2017
+};
